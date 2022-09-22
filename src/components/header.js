285f8f92ad9cb45c3922ede8f03a8b612f8edba4
Aleazar yilma <yilma4060@gmail.com>
2022-09-22 20:02:53 +0300

    add style

--- src/components/header.js
+++ src/components/header.js
@@ -2,16 +2,26 @@ import './header.css';
 import { Link } from 'react-router-dom';
 
 const Header = () => (
-  <nav className="panel-bg">
-    <span className="nav-logo">Bookstore CMS</span>
-    <ul>
-      <li>
-        <Link to="/" className="nav-books"> Books</Link>
-      </li>
-      <li>
-        <Link to="/catagory" className="nav-category">Category</Link>
-      </li>
-    </ul>
-  </nav>
+  <div className="header">
+    <nav className="panel-bg">
+      <span className="nav-logo">Bookstore CMS</span>
+      <ul>
+        <li>
+          <Link to="/" className="nav-books">
+            {' '}
+            Books
+          </Link>
+        </li>
+        <li>
+          <Link to="/catagory" className="nav-category">
+            Category
+          </Link>
+        </li>
+      </ul>
+    </nav>
+    <div>
+      <div className="ovalheader" />
+    </div>
+  </div>
 );
 export default Header;
